0e002f11b55d1de82718e7259ee832d3445532d7
Nishanth Vepachedu <nishant.vepachedu@gmail.com>
2014-04-27 21:19:09 -0400

    Completed the matrix inverse cache code for the Programming assignment 2 in R Programming course

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,47 @@
-## Put comments here that give an overall description of what your
-## functions do
+## functions to implement creation a cached matrix inverse closure
 
-## Write a short comment describing this function
-
+## makeCacheMatrix function is a closure that implements a simple cache method of saving the inverse of a matrix.
+## the getinv and setinv functions act as the set 
 makeCacheMatrix <- function(x = matrix()) {
-  #Just a comment
+  # define matrix to store the inverse - this is the cached value. initialize to a NA valued matrix
+  m_inv <- matrix(,nrow(x),ncol(x))
+  
+  # Sets the value of the matrix variable "x" as the argument passed in and resets the value of the inverse matrix "m_inv"
+  set <- function(y){
+    x <<-y
+    m_inv <<- matrix(,nrow(x),ncol(x))
+  }
+  
+  # returns the value of the matrix variable "x"
+  get <- function() x
+  
+  # sets the value of the inverse matrix as the argument passed in
+  setinv <- function(arginv) m_inv <<- arginv 
+  
+  # returns the value of the inverse matrix
+  getinv <- function() m_inv
+  
+  # return a list of functions defined in the closure to be operated on the matrix
+  list(set=set, get=get, setinv=setinv, getinv=getinv)
 }
 
 
-## Write a short comment describing this function
-
+## Gets the cached matrix inverse value for a makeCacheMatrix closure
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  # get the cached inverse matrix value of argument x
+  m_inverse <- x$getinv()
+  
+  # if the value of the inverse matrix is all NA, the cache function needs to set the inverse matrix value 
+  if(length(m_inverse[!is.na(m_inverse)]) > 0){
+    message("Using the cached inverse matrix")
+    return(m_inverse)
+  }
+  
+  # as the m_inverse is NULL, the inverse has never been computed for this x
+  # run solve and save the inverse as the cached matrix inverse value and return it
+  matval <- x$get()
+  m_inverse <- solve(matval)  
+  x$setinv(m_inverse)
+  
+  m_inverse
 }
